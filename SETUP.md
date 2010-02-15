13c0ce2e769c7ae6f77f1fcb42f059c501ce3164
Robert Kieffer <robert@broofa.com>
2010-02-15 05:35:07 -0800

    Working on Vlad deploy script

--- SETUP.md
+++ SETUP.md
@@ -6,6 +6,19 @@
 
 ## Set up local dev environment
 
-  * sudo apt-get install sqlite3 solr-common ruby rubygems
+  * sudo apt-get install sqlite3 libsqlite3-dev solr-common git-core ruby rubygems rake
+  * sudo apt-get ruby-dev
+  * sudo apt-get libopenssl-ruby # For "net/https" requires
+
+  * sudo gem install vlad vlad-git
+  * sudo gem install rails -v 2.3.4
+  * sudo gem install hpricot
+  * sudo gem install sqlite3-ruby
+
   * Follow calagator INSTALL.md instructions
+
+  * After pulling calagaor source, be sure to do "sudo rake gems:install" to get all the gems you'll need
+
+  * __NOTE__: You must *not* have the sqlite3 gem installed, otherwise db:migrate will fail
+
   * Get geocoder\_API.yml and secrets.yml files from one of the other devs (these aren't tracked in git)
--- config/deploy.rb
+++ config/deploy.rb
@@ -1,25 +1,33 @@
 set :application, "bendcal"
-set :repository,  "/Users/kieffer/projects/calagator"
+set :user, "calagator"
+set :domain, "#{user}@bendcal.org"
 
-set :scm, :git
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
-
-role :web, "bendcal.org"                          # Your HTTP server, Apache/etc
-role :app, "bendcal.org"                          # This may be the same as your `Web` server
-role :db,  "bendcal.org", :primary => true # This is where Rails migrations will run
-#role :db,  "your slave db-server here"
+set :repository,  "git://github.com/broofa/calagator.git"
 
 set :deploy_to, "/home/calagator/bendcal"
-set :user, "calagator"
 
-# If you are using Passenger mod_rails uncomment this:
-# if you're still using the script/reapear helper you will need
-# these http://github.com/rails/irs_process_scripts
+namespace :vlad do
+  desc "Full deployment of BendCal"
+  task "deploy" => %w[
+    vlad:update
+    vlad:migrate
+  ]
 
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+  task :update do
+    # Upload non-SCM'ed config files
+    ENV['FILES'] = 'config/theme.txt, config/secrets.yml, config/geocoder_api_keys.yml'
+    puts "Uploading " + ENV['FILES']
+    Rake::Task['vlad:upload'].invoke
+  end
+
+  desc "Start solr server"
+  remote_task :solr_start do
+    run "cd #{current_path}; rake solr:start RAILS_ENV=#{rails_env}"
+  end
+
+  desc "Stop solr server"
+  remote_task :solr_stop do
+    run "cd #{current_path}; rake solr:stop RAILS_ENV=#{rails_env}"
+  end
+end
+
